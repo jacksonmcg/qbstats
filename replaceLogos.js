a06c2cbb7e2ce380d0bac1c10bdc60ee670b35ca
Jackson McGregor <jmcgreg@g.clemson.edu>
2023-09-07 15:45:16 -0400

    Test

---
+++ replaceLogos.js
@@ -0,0 +1,342 @@
+function replaceLogos(x, td2){
+  if(x.includes("Chiefs")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Chiefs.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+  else if(x.includes("Bills")) {
+    // Create an image element
+    var img = document.createElement("img");
+    img.src = "logos/Bills.png"; // Replace with the actual path to the image
+
+    // Store the original content of column two and set the image as content
+    td2.setAttribute("data-original-content", td2.textContent);
+    td2.textContent = "";
+    td2.appendChild(img);
+  }
+  else if(x.includes("Bears")) {
+    // Create an image element
+    var img = document.createElement("img");
+    img.src = "logos/Bears.png"; // Replace with the actual path to the image
+
+    // Store the original content of column two and set the image as content
+    td2.setAttribute("data-original-content", td2.textContent);
+    td2.textContent = "";
+    td2.appendChild(img);
+  }
+  else if(x.includes("49ers")) {
+    // Create an image element
+    var img = document.createElement("img");
+    img.src = "logos/49ers.png"; // Replace with the actual path to the image
+
+    // Store the original content of column two and set the image as content
+    td2.setAttribute("data-original-content", td2.textContent);
+    td2.textContent = "";
+    td2.appendChild(img);
+  }
+    else if(x.includes("Eagles")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Eagles.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Cowboys")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Cowboys.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Commanders")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Commanders.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Giants")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Giants.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Falcons")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Falcons.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Rams")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Rams.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Bengals")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Bengals.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Browns")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Browns.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Steelers")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Steelers.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Ravens")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Ravens.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("2 Teams") || x.includes("3 Teams")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/NFL.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Dolphins")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Dolphins.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Jets")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Jets.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Patriots")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Patriots.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Packers")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Packers.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Panthers")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Panthers.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Broncos")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Broncos.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Cardinals")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Cardinals.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Chargers")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Chargers.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Lions")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Lions.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Vikings")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Vikings.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Raiders")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Raiders.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Texans")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Texans.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Saints")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Saints.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Titans")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Titans.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Buccaneers")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Buccaneers.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Colts")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Colts.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Seahawks")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Seahawks.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Jaguars")) {
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/Jaguars.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+    else if(x.includes("Bison") || x.includes("Yanks")){
+      // Create an image element
+      var img = document.createElement("img");
+      img.src = "logos/0.png"; // Replace with the actual path to the image
+
+      // Store the original content of column two and set the image as content
+      td2.setAttribute("data-original-content", td2.textContent);
+      td2.textContent = "";
+      td2.appendChild(img);
+    }
+}
